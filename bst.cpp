f07c05ecd97f0721522b03e7a38acb7476336126
meenal1305 <100236356+meenal1305@users.noreply.github.com>
2022-10-07 21:19:21 +0530

    Create bst.cpp

---
+++ bst.cpp
@@ -0,0 +1,59 @@
+//implementation of BST
+class node
+{
+    public:
+    int val;
+    node*left;
+    node*right;
+    node(int x)
+    {
+        val=x;
+        left=right=NULL;
+    }
+};
+node*createBST(node*root, int x)
+{
+    if(root==NULL)
+    {
+    node*newNode=new node(x);
+        return newNode; 
+    }
+    if(x<=root->val)
+    {
+        root->left=createBST(root->left, x);
+    }
+    else
+    {
+        root->right=createBST(root->right, x);
+    }
+    return root;
+}
+void preOrder(node* root)
+{
+    if(root == NULL)
+        return;
+    
+    preOrder(root->left);
+        cout<<root->val<<" ";
+    preOrder(root->right);
+}
+
+int main() {
+    
+    int n; cin>>n;
+    
+    node* root = NULL;
+    for(int i = 0;i<n;i++)
+    {
+        int x; cin>>x;
+        
+        root = createBST(root, x);
+    }
+    
+    preOrder(root);
+
+}
+
+
+
+
